4c8220b3eeb3969905562329cb3f37bd2bf92f6b
vagrid <vagrid@zaclys.net>
2025-03-27 20:13:40 +0100

    ✨ feat: add route docs to show URL dispatch

--- gator/src/main.rs
+++ gator/src/main.rs
@@ -4,6 +4,10 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().body("OK")
 }
 
+async fn docs() -> impl Responder {
+    "docs"
+}
+
 #[get("/")]
 async fn home() -> impl Responder {
     HttpResponse::Ok().body("Home")
@@ -14,6 +18,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(home)
             .route("/healthcheck", web::get().to(health_check))
+            .service(web::scope("/internals").route("/docs", web::get().to(docs)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
