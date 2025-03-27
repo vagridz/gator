5aed29afae45b89b9656d3756dc5032fbaf3fa58
vagrid <vagrid@zaclys.net>
2025-03-27 17:19:44 +0100

    ✨ feat: add http web server
    add / and /healthcheck routes

--- gator/src/main.rs
+++ gator/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Home")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+            .route("/healthcheck", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
